8651efe2f706007067be5aafaf16327bc78c2dd1
靳宏立(Eleven Jin) <jinhongli@qiyi.com>
2017-11-24 15:32:07 +0800

    add Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import Header from './component/Header';
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -39,9 +41,7 @@ class BooksApp extends React.Component {
           </div>
         ) : (
           <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+            <Header />
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+<section className="hero is-dark">
+  <div className="hero-body">
+    <div className="container">
+      <h1 className="title has-text-centered">
+        My Reads
+      </h1>
+    </div>
+  </div>
+</section>
+)
+
+export default Header;
