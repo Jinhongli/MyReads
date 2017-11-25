197483a64674238fb067b20f264cb5269faa92a7
jinhongli <jhl211@163.com>
2017-11-25 16:36:09 +0800

    change Book style & hover to show BookControl

--- src/component/Book.js
+++ src/component/Book.js
@@ -12,21 +12,43 @@ class Book extends React.Component {
     }).isRequired,
   };
 
+  state = {
+    showControl: false,
+  };
+
+  handleHover() {
+    this.setState(prevState => {
+      return {
+        showControl: !prevState.showControl,
+      };
+    });
+  }
+
   render() {
-    const { info: {name, authors, cover}, color } = this.props;
-    const bookCoverUrl = {
-      backgroundImage: `url(${cover || ''})`,
-    };
+    const { info: { name, authors, cover }, color } = this.props;
     return (
       <div className="column">
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={bookCoverUrl} />
-          <BookControl color={color} />
+        <div
+          className="card book"
+          onMouseEnter={() => this.handleHover()}
+          onMouseOut={() => this.handleHover()}
+        >
+          <div className="card-image">
+            <figure className="image">
+              <img src={cover} alt={name} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="content">
+              <span className="is-size-7">{name}</span>
+              <br />
+              <span className="is-size-7 has-text-grey-light">
+                {authors.join(', ')}
+              </span>
+            </div>
+          </div>
+          {this.state.showControl && <BookControl color={color} />}
         </div>
-        <div className="book-title">{name}</div>
-        <div className="book-authors">{authors.join(', ')}</div>
-      </div>
       </div>
     );
   }
